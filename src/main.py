2670df6174936f60b1357ea51e643aca20b4aea6
Tarak Sristy <sristy@wisc.edu>
2023-10-27 03:54:59 -0500

    Added error handling for missing or empty data and increased delay to 1 hour

--- src/main.py
+++ src/main.py
@@ -1,28 +1,32 @@
-import os
 import requests
-import sqlite3
 import pandas as pd
-import matplotlib.pyplot as plt
 import time
 
 
 main_df = None
 
+
 def get_data():
     global main_df
     try:
-         main_df = pd.read_csv('main_data.csv')
+        main_df = pd.read_csv("main_data.csv")
     except FileNotFoundError:
         print("File not found, creating new file")
-        main_df = pd.DataFrame() 
- 
+        main_df = pd.DataFrame()
+
     # Get data from API
-    url = "https://www.csgo.com.cn/api/lotteryHistory"
-    response = requests.get(url)
-    data = response.json()
+    try:
+        url = "https://www.csgo.com.cn/api/lotteryHistory"
+        response = requests.get(url)
+        data = response.json()
+    except:
+        print("ERROR: Could not get data from API")
+        return None
 
     # Create dataframe
     df = pd.json_normalize(data, record_path="result")
+    if df.empty:
+        return None
 
     # Drop timestamp and out column, rename src to case
     df = df.drop(["timestamp", "out"], axis=1)
@@ -30,23 +34,30 @@ def get_data():
 
     return df
 
-# get data every 30 minutes, if new data is available
+
+# get data every 1 hour, if new data is available
 time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("Starting at " + time_str)
 
 for i in range(20):
     time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    df = get_data()
+
+    try:
+        df = get_data()
+    except df is None:
+        print("No data found " + time_str)
+        time.sleep(3600)
+        continue
 
     # add to main dataframe, drop duplicates
     main_df = pd.concat([main_df, df], ignore_index=True)
 
-    if(main_df.duplicated().any()):
+    if main_df.duplicated().any():
         print("No new data " + time_str)
         main_df = main_df.drop_duplicates()
     else:
-        print("New data available at " + time_str)
+        print("New data available " + time_str)
 
-    main_df.to_csv('main_data.csv', index=False)
+    main_df.to_csv("main_data.csv", index=False)
 
-    time.sleep(1800)
+    time.sleep(3600)
